eadaf2635a502967cd88a1fac75cd0af09bf6285
rkddbs1031 <rkddbs1031@conbridge.co.kr>
2022-05-20 00:16:39 +0900

    mod: searchPage  레이아웃 수정

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,16 +1,19 @@
 import Input from './main/input'
+import Result from './main/result'
 import styles from './SearchPage.module.scss'
 
 const SearchPage = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.containerBox}>
-        <div className={styles.title}>
-          <p>국내 모든 임상시험 검색하고</p>
-          <p>온라인으로 참여하기</p>
-        </div>
-        <Input />
-      </div>
+      <section className={styles.section1}>
+        <h2 className={styles.title}>
+          국내 모든 임상시험 검색하고
+          <br />
+          온라인으로 참여하기
+        </h2>
+      </section>
+      <Input />
+      <Result />
     </div>
   )
 }
