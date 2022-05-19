dc373b924c7001806ecf331142c716b499b4b8b3
jihun1233 <leejihun1233@gmail.com>
2022-05-20 02:56:37 +0900

    Update: disease관련 타입 파일이름 및 타입이름 변경
    - .env.copy 현재 불필요한 부분 삭제

---
+++ src/types/disease.d.ts
@@ -0,0 +1,31 @@
+export interface IDiseaseParams {
+  searchText: string | undefined
+}
+
+export interface IDiseaseItem {
+  sickCd: string
+  sickNm: string | undefined
+}
+
+// export interface IDiseaseItems {
+//   item: IDiseaseItem[]
+// }
+
+export interface IBody {
+  items: { item: IDiseaseItem[] }
+  numOfRows: number
+  pageNo: number
+  totalCount: number
+}
+
+export interface IHeader {
+  resultCode: number
+  resultMsg: string
+}
+
+export interface IDiseaseResponse {
+  response: {
+    body: IBody
+    header: IHeader
+  }
+}
