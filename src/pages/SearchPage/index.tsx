fb4b473bc312982f45ccda286538482189487288
markyul <pig68111000@gmail.com>
2022-05-22 04:20:26 +0900

    main에 h2를 감싸는 section 삭제

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -5,13 +5,11 @@ import styles from './SearchPage.module.scss'
 const SearchPage = () => {
   return (
     <div className={styles.container}>
-      <section>
-        <h2 className={styles.title}>
-          국내 모든 임상시험 검색하고
-          <br />
-          온라인으로 참여하기
-        </h2>
-      </section>
+      <h2>
+        국내 모든 임상시험 검색하고
+        <br />
+        온라인으로 참여하기
+      </h2>
       <Input />
       <Result />
     </div>
