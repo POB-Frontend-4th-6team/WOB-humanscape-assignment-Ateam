c6ab48887d4d3f6b037f70bc25c264065ece641f
rkddbs1031 <rkddbs1031@gmail.com>
2022-05-22 01:57:38 +0900

    Mod: Header 컴포넌트 폴더명 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Header from './components/header'
+import Header from './components/Header'
 import SearchPage from './SearchPage'
 import Footer from './components/Footer'
 import styles from './Pages.module.scss'
